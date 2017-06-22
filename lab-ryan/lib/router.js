512594fc1c1e4530b96d65f83600b79442f90e54
Ryan Turner <ryanthepit@gmail.com>
2017-06-22 11:06:04 -0700

    started building router

---
+++ lab-ryan/lib/router.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const requestPars = require('./request-parse.js');
+
+const routes = {
+  GET: { },
+  PUT: {},
+  POST: {},
+  DELETE: {},
+};
+
+const router = module.exports = {};
