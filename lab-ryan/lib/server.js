5b2a3b99f3c106d56fe098869639ad89dbe171e6
Ryan Turner <ryanthepit@gmail.com>
2017-06-22 16:58:51 -0700

    get method is working

--- lab-ryan/lib/server.js
+++ lab-ryan/lib/server.js
@@ -2,61 +2,79 @@
 
 const http = require('http');
 const router = require('./router.js');
+const requestParse = require('./request-parse.js');
 const Article = require('../models/article.js');
 
 let storage = {};
 
-router.get('/hello', (req, res) => {
-  res.write('yeyyeyeyye');
-  res.end();
-});
 
 router.post('/api/article', (req, res) => {
   console.log('hit /api/notes');
-  if(!req.body.content){
-    res.write(400);
+  if(!req.body){
+    res.writeHead(400);
     res.end();
     return;
   }
+  let newArticle = new Article (req.body.title, req.body.author);
+  article.id = uuvd.v1();
+  storage[newArticle.id] = newArticle;
 
-  // uuid  generate a random string that will not likely conflict with a future random string
-  let note = {
-    id: uuid.v1(),
-    content: req.body.content,
-  };
-
-  storage[note.id] = note;
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
+  res.writeHead(201, {
+    'Content-type' : 'application/json',
   });
-  res.write(JSON.stringify(note));
+  res.write(JSON.stringify(newArticle));
   res.end();
+  return;
 });
 
-router.get('/api/notes', (req, res) => {
+// uuid  generate a random string that will not likely conflict with a future random string
+router.get('/api/articles', (req, res) => {
   if(!req.url.query.id){
     res.writeHead(400);
+    res.write('bad request');
     res.end();
-    return ;
+    return;
   }
-
   if(!storage[req.url.query.id]){
     res.writeHead(404);
     res.end();
-    return ;
+    return;
   }
-
-  res.writeHead(200, {
-    'Content-Type': 'application/json',
-  });
-
-  res.write(JSON.stringify(storage[req.url.query.id]));
-  res.end();
-
+  if(req.url.query.id){
+    res.writeHead(200, {
+      'Content-type' : 'application/json',
+    });
+    res.write(JSON.stringify(storage[req.url.query.id]));
+    res.end();
+  }
 });
 
-// create server
-const server = module.exports = http.createServer(router.route);
+// 
+//
+// router.put('/api/notes', (req, res) => {
+//   if(!req.url.query.id){
+//     res.writeHead(400);
+//     res.end();
+//     return ;
+//   }
+//
+//   if(!storage[req.url.query.id]){
+//     res.writeHead(404);
+//     res.end();
+//     return ;
+//   }
+//
+//   res.writeHead(200, {
+//     'Content-Type': 'application/json',
+//   });
+//
+//   res.write(JSON.stringify(storage[req.url.query.id]));
+//   res.end();
+//
+// });
+//
+// // create server
+// const server = module.exports = http.createServer(router.route);
 
 
 // how we did it first
