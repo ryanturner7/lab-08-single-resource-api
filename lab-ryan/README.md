96144c24002a61ec526afad6fe6cd188ef86f6a2
Ryan Turner <ryanthepit@gmail.com>
2017-06-22 18:54:11 -0700

    added README

---
+++ lab-ryan/README.md
@@ -0,0 +1,3 @@
+#Single recourse api
+
+In this application, I created a single resource API that allows a user to create, read, update, and destroy articles using GET, POST, PUT, and DELETE methods.
--- lab-ryan/test/test-index.js
+++ lab-ryan/test/test-index.js
@@ -2,19 +2,53 @@
 
 const superagent = require('superagent');
 const expect = require('expect');
-const server = require('server');
+const Article = require('../models/article.js')
+const server = require('../lib/server.js');
+let tempArticle;
 
-describe('testing routes', (){
+describe('testing article routes', function(){
   before((done) => {
-    server.listen(3000, () => {
-      console.log('server is up for testing');
-      done();
-    });
+    server.listen(3000, () => done());
   });
   after((done) => {
-    server.close(() => {
-      console.log('testing finished');
-      done();
-    })
-  })
-}
+    server.close(() => done());
+  });
+  describe('testing POST /api/notes', () => {
+    it('should respond with an article', (done) => {
+      superagent.post('localhost:3000/api/articles')
+        .send(JSON.stringify({title: 'The art of coding', author: 'Ryan Turner'}))
+        .end((err, res) => {
+          if (err) return done(err);
+          expect(res.status).toEqual(200);
+          expect(res.body.id).toExist();
+          expect(res.body.title).toEqual('The art of coding');
+          expect(res.body.author).toEqual('Ryan Turner');
+          tempArticle = res.body;
+          done();
+        });
+    });
+
+    it('should respond with a 400 bad request', (done) => {
+      superagent.post('localhost:3000/api/articles')
+        .send({})
+        .end((err, res) => {
+          expect(res.status).toEqual(400);
+          done();
+        });
+    });
+  });
+
+  // describe('testing GET /api/notes', () => {
+  //   it('should respond with a note', (done) => {
+  //     superagent.get(`localhost:3000/api/notes?id=${tempNote.id}`)
+  //     .end((err, res) => {
+  //       if (err) return done(err)
+  //       expect(res.status).toEqual(200)
+  //       expect(res.body.id).toEqual(tempNote.id)
+  //       expect(res.body.content).toEqual('example data')
+  //       tempNote = res.body;
+  //       done()
+  //     })
+  //   })
+  // });
+});
