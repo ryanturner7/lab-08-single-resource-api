a8f7aa73113e5377a608c2ee87df6b1616cc1718
Ryan Turner <ryanthepit@gmail.com>
2017-06-22 17:29:17 -0700

    GET PUT and DELETE methods are working

--- lab-ryan/lib/server.js
+++ lab-ryan/lib/server.js
@@ -3,10 +3,9 @@
 const http = require('http');
 const router = require('./router.js');
 const requestParse = require('./request-parse.js');
+const uuid = require('uuid');
+const storage = require('../model/storage.js');
 const Article = require('../models/article.js');
-
-let storage = {};
-
 
 router.post('/api/article', (req, res) => {
   console.log('hit /api/notes');
@@ -27,14 +26,14 @@ router.post('/api/article', (req, res) => {
   return;
 });
 
-// uuid  generate a random string that will not likely conflict with a future random string
-router.get('/api/articles', (req, res) => {
+router.get('/api/article', (req, res) => {
   if(!req.url.query.id){
     res.writeHead(400);
     res.write('bad request');
     res.end();
     return;
   }
+
   if(!storage[req.url.query.id]){
     res.writeHead(404);
     res.end();
@@ -48,36 +47,35 @@ router.get('/api/articles', (req, res) => {
     res.end();
   }
 });
-
-// 
-//
-// router.put('/api/notes', (req, res) => {
-//   if(!req.url.query.id){
-//     res.writeHead(400);
-//     res.end();
-//     return ;
-//   }
-//
-//   if(!storage[req.url.query.id]){
-//     res.writeHead(404);
-//     res.end();
-//     return ;
-//   }
-//
-//   res.writeHead(200, {
-//     'Content-Type': 'application/json',
-//   });
-//
-//   res.write(JSON.stringify(storage[req.url.query.id]));
-//   res.end();
-//
-// });
-//
-// // create server
-// const server = module.exports = http.createServer(router.route);
-
-
-// how we did it first
-//http.createServer((req, res) => {
-  //router.route(req,res)
-//})
+router.put('/api/articles', (req, res) => {
+  let body = req.body;
+  if(!body || !body.title || !body.author){
+    res.write(400);
+    res.end();
+    return;
+  }
+  res.writeHead(202, {
+    'Content-type' : 'application/json',
+  });
+  storage[req.url.query.id].title = req.body.title;
+  storage[req.url.query.id].author = req.body.author;
+    res.writeHead(JSON.stringify(storage[req.url.query.id]));
+    res.end();
+    return;
+  })
+  router.delete('/api/article', (req, res) => {
+    if(!storage[req.url.query.id]){
+      res.writeHead(404);
+      res.write('invalid id');
+    });
+      res.end();
+      return;
+    }
+    delete storage[req.url.query.id];
+    res.writeHead(204, {
+      'Content-type' : 'application/json',
+  });
+res.write(JSON.stringify(storage[req.url.query.id]));
+res.end();
+});
+const serverr = module.exports = http.createServer(router.route);
